5f002ae50259c8a7ac0f72b1311c0f0dec6f8373
Samarth-170904 <samarthmule1704@gmail.com>
2025-06-16 15:24:09 +0530

    libraries are imported and set up done

---
+++ run_analysis.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import classification_report, roc_auc_score, confusion_matrix
+
+print("Libraries imported successfully.")
